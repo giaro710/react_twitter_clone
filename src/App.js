5669e3fc1ac0e6e886bd1b3f844c53439e929002
Gian Marco Romani <giaro710@gmail.com>
2020-11-18 21:29:03 +0100

    some clening

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <h1>Twitter Clone</h1>
+    </div>
+  );
 };
 
 export default App;
