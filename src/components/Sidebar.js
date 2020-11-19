936b236287125bf350b51b240a8e0eb58f5b3b07
Gian Marco Romani <giaro710@gmail.com>
2020-11-19 10:42:23 +0100

    Almost finished the skeleton and the style of the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,14 +4,15 @@ import SidebarOption from "./SidebarOption";
 
 import "../css/Sidebar.css";
 import icons from "../icons";
+import { Button } from "@material-ui/core";
 
 const Sidebar = () => {
   return (
     <div>
       <div className="sidebar">
-        <icons.twitter />
+        <icons.twitter className="sidebar__twitter-icon" />
 
-        <SidebarOption Icon={icons.home} text="Home" />
+        <SidebarOption active Icon={icons.home} text="Home" />
         <SidebarOption Icon={icons.search} text="Explore" />
         <SidebarOption Icon={icons.notifications} text="Notifications" />
         <SidebarOption Icon={icons.mail} text="Mail" />
@@ -19,6 +20,10 @@ const Sidebar = () => {
         <SidebarOption Icon={icons.list} text="List" />
         <SidebarOption Icon={icons.identity} text="Notifications" />
         <SidebarOption Icon={icons.more} text="Notifications" />
+
+        <Button variant="outlined" className="sidebar__tweet" fullWidth>
+          Tweet
+        </Button>
       </div>
     </div>
   );
--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "../css/SidebarOption.css";
 
-const SidebarOption = ({ Icon, text }) => {
+const SidebarOption = ({ Icon, text, active }) => {
   return (
-    <div className="sidebarOption">
-      <Icon />
-      <h2>{text}</h2>
+    <div className={`sidebarOption ${active && "sidebarOption--active"} `}>
+      <Icon className="sidebarOption__icon" />
+      <h2 className="sidebarOption__label">{text}</h2>
     </div>
   );
 };
