10179333b8261f59f386dadd8fa90b03ec856aaf
Gian Marco Romani <giaro710@gmail.com>
2020-11-19 18:18:51 +0100

    Done the style of the Widgets component

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,17 +1,39 @@
 import React from "react";
 import "../css/Widgets.css";
+import icons from "../icons";
 
 import {
-  TwitterTimeEmbed,
-  TwitterShare,
-  Button,
+  TwitterTimelineEmbed,
+  TwitterShareButton,
   TwitterTweetEmbed,
 } from "react-twitter-embed";
 
 const Widgets = () => {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input-container">
+        <icons.search className="widgets__searchIcon" />
+        <input
+          type="text"
+          placeholder="Search Twitter"
+          className="widgets__input"
+        />
+      </div>
+      <div className="widgets__container">
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId={"858551177860055040"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="cleverqazi"
+          options={{ height: "400" }}
+        />
+
+        <TwitterShareButton
+          url={"https://www.facebook.com/profile.php?id=100009346730690"}
+          options={{ text: "I love react", via: "Gian Marco Romani" }}
+        />
+      </div>
     </div>
   );
 };
